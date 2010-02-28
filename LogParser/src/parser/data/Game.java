e51f11b473d1e086b10d896bb7ae59e1c07016bb
maryann.turgeon <maryann.turgeon@94369782-0f85-11df-ac45-33b0ac6a2133>
2010-02-28 00:48:36 +0000

    Struct de données que le parser enverra au main (première esquisse, pas final)

---
+++ LogParser/src/parser/data/Game.java
@@ -0,0 +1,11 @@
+package parser.data;
+
+public class Game {
+
+	// public Integer idGame = null;; // Game identifier
+	public String startTime = null;; // Game start time
+	public String dealerName = null;; // The player who has the button
+	public String[] table = null;; // Array of 5 elements representing the community cards
+	public Bets[] bettingRounds = null;;
+
+}
---
+++ LogParser/src/parser/data/GameSet.java
@@ -0,0 +1,12 @@
+package parser.data;
+
+import java.util.ArrayList;
+
+public class GameSet extends ArrayList<Game> {
+
+	public Integer nbPlayers = null;
+	public Double smallBlindValue = null;
+	public Double bigBlindValue = null;
+	public String gameType = null;
+
+}
