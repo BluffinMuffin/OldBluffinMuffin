084fe6840345c5541e0847006e7358e15cc83b1b
maryann.turgeon <maryann.turgeon@94369782-0f85-11df-ac45-33b0ac6a2133>
2010-03-08 17:40:55 +0000

    TODO: Fix le regex du winner

--- LogParser/src/parser/FullTiltParser.java
+++ LogParser/src/parser/FullTiltParser.java
@@ -11,20 +11,17 @@ import db.PokerLogsDBManager;
 
 public class FullTiltParser extends AbsParser {
 
-	private static final FullTiltParser INSTANCE = new FullTiltParser();
+	// private static final FullTiltParser INSTANCE = new FullTiltParser();
 
-	public static FullTiltParser getInstance() {
-		return INSTANCE;
-
-	}
+	// public static FullTiltParser getInstance() {
+	// return INSTANCE;
+	//
+	// }
 
 	private PokerLogsDBManager DBManager;
 
 	private HashMap<String, Integer> playerMap = new HashMap<String, Integer>();
 	private GameSet gameSet = new GameSet();
-
-	// To be initialised at the start of each individual game
-	private Game game = null;
 
 	@Override
 	void understand(String fileContent) {
@@ -40,32 +37,33 @@ public class FullTiltParser extends AbsParser {
 
 			rounds = log.split("\r\n\\*\\*\\*.");
 
-			game = new Game(); // Instance a new game
+			Game game = new Game(); // Instance a new game
 
 			for (int x = 0; x < rounds.length; x++) {
 
 				if (rounds[x].length() > 0) {
 					if (x == 0) { // Si on est dans la section info générales de la game
 
-						// setPlayers(rounds[x]);
-						// readHeader((rounds[x].split("\r\n"))[0]);
-						//
-						// setDealerID(rounds[x]);
-
-						setBlindBettingRound(rounds[x], "big"); // Set the BIG blinds
-						setBlindBettingRound(rounds[x], "small"); // Set the SMALL blinds
+						setPlayers(rounds[x]);
+						readHeader((rounds[x].split("\r\n"))[0]);
+						setDealerID(rounds[x], game);
+						setBlindBettingRound(rounds[x], "big", game); // Set the BIG blinds
+						setBlindBettingRound(rounds[x], "small", game); // Set the SMALL blinds
 
 					} else if (x == rounds.length - 1) {
 						// Game Summary
 
 						// Déterminer le gagnant et le montant amassé
+						setWinner(rounds[x], game);
 						// Remplir la table avec le "Board : "
-
-						// System.exit(-1);
+						setBoard(rounds[x], game);
+						System.exit(-1);
 
 					} else {
 						// is typical betting round
-						insertBettingActions(rounds[x]);
+
+						insertBettingActions(rounds[x], game);
+
 					}
 
 				}
@@ -74,42 +72,63 @@ public class FullTiltParser extends AbsParser {
 
 	}
 
-	private void insertBettingActions(String bettingInfo) {
+	private void setWinner(String summary, Game game) {
+
+		Pattern pot = Pattern.compile("Total pot \\$?(\\d+[.]?[0-9]?{2}) \\| Rake \\$?(\\d+[.]?[0-9]?{2})");
+		// Pattern winner = Pattern.compile("Seat \\d: ([\\w+\\s]+) (.+)?collected \\(\\$?(\\d+[.]?[0-9]?{2})\\), (\\w+)");
+		Pattern winner = Pattern.compile("Seat\\s\\d:\\s(\\w+\\s)+(?:\\((\\w+\\s)+\\))?(\\(.*\\)),(.*)");
+		Matcher potMatcher = pot.matcher(summary);
+		Matcher winMatcher = winner.matcher(summary);
+
+		// TODO: Find a split pot to allow multiple wins
+		if (potMatcher.find()) {
+			System.out.println("Has pot");
+
+		}
+
+		while (winMatcher.find()) {
+			for (int i = 0; i < winMatcher.groupCount(); i++) {
+				System.out.println(i + " : " + winMatcher.group(i));
+			}
+			// System.out.println(winMatcher.group(0));
+			// System.out.println(winMatcher.group(1));
+			// System.out.println(winMatcher.group(2));
+			// System.out.println(winMatcher.group(3));
+		}
+
+	}
+
+	private void setBoard(String summary, Game game) {
+		// Can wait until summary when we have Board [.. .. .. .. ..]
+		// Pattern tableCards = Pattern.compile("([\\w+\\s]+)\\*\\*\\* \\x5B(..) ?(..)? ?(..)?\\x5D");
+		Pattern board = Pattern.compile("Board: \\[(..) (..) (..) ?(..)? ?(..)?\\]");
+		Matcher tableMatcher = board.matcher(summary);
+
+		if (tableMatcher.find()) {
+
+			game.table[0] = tableMatcher.group(1);
+			game.table[1] = tableMatcher.group(2);
+			game.table[2] = tableMatcher.group(3);
+			game.table[3] = tableMatcher.group(4);
+			game.table[4] = tableMatcher.group(5);
+
+		}
+
+	}
+
+	private void insertBettingActions(String bettingInfo, Game game) {
 
 		String[] actions = (bettingInfo.split("\r\n"));
 		String round = getBettingRoundType(actions[0]);
+		Double minBid = gameSet.bigBlindValue;
 
 		// TODO: Replace by megaMatcher of DOOOMM!!
 		/*
 		 * Pattern fold = Pattern.compile("([\\w+\\s]+) folds"); Pattern raise = Pattern.compile("([\\w+\\s]+)raises to \\$?(\\d+[.]?[0-9]?{2})"); Pattern call = Pattern.compile("([\\w+\\s]+) calls \\$?(\\d+[.]?[0-9]?{2})"); Pattern check = Pattern.compile(""); Pattern voluntaryBet = Pattern.compile("([\\w+\\s]+)adds \\$?(\\d+[.]?[0-9]?{2})");
 		 */
 
-		Pattern cardsDealt = Pattern.compile("Dealt to ([\\w+\\s]+)\\x5B(..) (..)\\x5D");
-		Pattern megaPattern = Pattern.compile("([\\w+\\s]+)(folds|raises|calls|adds|checks) ?(to )?\\$?(\\d+)?");
-
-		// Can wait until summary when we have Board [.. .. .. .. ..]
-		// Pattern tableCards = Pattern.compile("([\\w+\\s]+)\\*\\*\\* \\x5B(..) ?(..)? ?(..)?\\x5D");
-		// Matcher tableMatcher = tableCards.matcher(actions[0]);
-		//
-		// if (!round.equals("HOLE CARDS")) {
-		// // FLOP / TURN /RIVER
-		// // Get the turned cards and add to game.table
-		// if (tableMatcher.find()) {
-		// if (tableMatcher.group(1).equals("FLOP")) {
-		// game.table[0] = tableMatcher.group(2);
-		// game.table[1] = tableMatcher.group(3);
-		// game.table[2] = tableMatcher.group(4);
-		//
-		// } else if (tableMatcher.group(1).equals("TURN")) {
-		// game.table[3] = tableMatcher.group(2);
-		//
-		// } else if (tableMatcher.group(1).equals("RIVER")) {
-		// game.table[4] = tableMatcher.group(2);
-		// }
-		//
-		// }
-		//
-		// }
+		Pattern cardsDealt = Pattern.compile("Dealt to ([\\w+\\s]+)\\[(..) (..)\\]");
+		Pattern megaPattern = Pattern.compile("([\\w+\\s]+)(folds|raises|calls|adds|checks) ?(to )?\\$?(\\d+[.]?[0-9]?{2})");
 
 		for (int x = 1; x < actions.length; x++) {
 			Matcher cardsDealtMatcher = cardsDealt.matcher(actions[x]);
@@ -125,7 +144,8 @@ public class FullTiltParser extends AbsParser {
 					Bet b = new Bet();
 					b.round = round;
 					b.playerName = megaMatcher.group(1);
-					b.amount = Double.valueOf(megaMatcher.group(4));
+					b.amount = Double.valueOf(megaMatcher.group(4)) - minBid;
+					minBid = Double.valueOf(megaMatcher.group(4));
 					b.action = Bet.ActionType.Raise;
 					game.add(b);
 
@@ -167,7 +187,7 @@ public class FullTiltParser extends AbsParser {
 
 	}
 
-	private void setDealerID(String info) {
+	private void setDealerID(String info, Game game) {
 
 		/*
 		 * TODO: Decide how to correspond seat # to player name
@@ -200,7 +220,7 @@ public class FullTiltParser extends AbsParser {
 		// group(11): timezone
 		// group(12): date
 
-		String tableInfo = "([\\w+\\s]+)Game.#(\\d+):(..+,)?.Table.(\\w+\\s)+([\\x28]\\d max[\\x29])? ?- ";
+		String tableInfo = "([\\w+\\s]+)Game.#(\\d+):(..+,)?.Table.(\\w+\\s)+([\\(]\\d max[\\)])? ?- ";
 		String bettype = "([\\w+\\s]+)(Hold'em|Omaha) - ";
 		String timestmp = "([0-9]{0,2}:[0-9]{0,2}:[0-9]{0,2}) (\\w+) - ([0-9]{0,4}\\/[0-9]{0,2}\\/[0-9]{0,2})";
 
@@ -237,7 +257,7 @@ public class FullTiltParser extends AbsParser {
 
 		Integer nbPlayers = 0;
 
-		Pattern myregex = Pattern.compile("Seat \\d: ([\\w+\\s]+)[\\x28]\\$?(.+)[\\x29]");
+		Pattern myregex = Pattern.compile("Seat \\d: ([\\w+\\s]+)[\\(]\\$?(.+)[\\)]");
 
 		Matcher matcher = myregex.matcher(seatInfo);
 
@@ -257,7 +277,7 @@ public class FullTiltParser extends AbsParser {
 
 	}
 
-	private void setBlindBettingRound(String info, String blindType) {
+	private void setBlindBettingRound(String info, String blindType, Game game) {
 
 		/*
 		 * TODO: Get corresponding ID's from playerMap once bd is up and running
@@ -277,7 +297,13 @@ public class FullTiltParser extends AbsParser {
 			System.out.println("id " + blindType + "Blind($" + RegexMatcher.group(2) + "): " + RegexMatcher.group(1));
 			Bet b = new Bet();
 			b.forced = true;
-			b.forceType = Bet.ForcedBetType.Blind;
+
+			if (blindType.equals("big")) {
+				b.forceType = Bet.ForcedBetType.BigBlind;
+			} else {
+				b.forceType = Bet.ForcedBetType.SmallBlind;
+			}
+
 			b.playerName = RegexMatcher.group(1);
 			b.amount = Double.valueOf(RegexMatcher.group(2));
 			game.add(b);
