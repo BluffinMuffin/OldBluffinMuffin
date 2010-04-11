55db65c2c82f5698cd3cb195b49593394e6ad81d
ericmas001 <ericmas001@94369782-0f85-11df-ac45-33b0ac6a2133>
2010-04-11 17:28:40 +0000

    C# debut BluffinPokerGui

---
+++ C#Integration/AppTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using BluffinPokerGUI.Lobby;
+
+namespace AppTest
+{
+    static class Program
+    {
+        /// <summary>
+        /// Point d'entrée principal de l'application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new AddTableForm(null,"Patate",5));
+        }
+    }
+}
---
+++ C#Integration/BluffinPokerGui/Lobby/AddTableForm.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using PokerWorld.Game;
+
+namespace BluffinPokerGUI.Lobby
+{
+    public partial class AddTableForm : Form
+    {
+        private readonly string m_PlayerName;
+
+        private bool m_OK;
+        private string m_TableName;
+        private int m_BigBlind;
+        private int m_NbPlayer;
+        private int m_WaitingTimeAfterPlayerAction;
+        private int m_WaitingTimeAfterBoardDealed;
+        private int m_WaitingTimeAfterPotWon;
+        private TypeBet m_Limit;
+
+        public TypeBet Limit
+        {
+            get { return m_Limit; }
+        }
+        public int WaitingTimeAfterPotWon
+        {
+            get { return m_WaitingTimeAfterPotWon; }
+        }
+        public int WaitingTimeAfterBoardDealed
+        {
+            get { return m_WaitingTimeAfterBoardDealed; }
+        }
+        public int WaitingTimeAfterPlayerAction
+        {
+            get { return m_WaitingTimeAfterPlayerAction; }
+        }
+        public int NbPlayer
+        {
+            get { return m_NbPlayer; }
+        }
+        public int BigBlind
+        {
+            get { return m_BigBlind; }
+        }
+        public string TableName
+        {
+            get { return m_TableName; }
+        }
+        public bool OK
+        {
+            get { return m_OK; }
+        }
+        public AddTableForm(Form owner, string playerName, int nbPlayers) : base()
+        {
+            m_OK = false;
+            Parent = owner;
+            m_PlayerName = playerName;
+            InitializeComponent();
+            txtTableName.Text = m_PlayerName + " Table";
+            foreach (string s in Enum.GetNames(typeof(TypeBet)))
+                clstGameLimit.Items.Add(s);
+            clstGameLimit.SelectedItem = TypeBet.NoLimit.ToString();
+            nudNbPlayers.Minimum = Math.Max(nbPlayers,2);
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            m_TableName = txtTableName.Text;
+            m_BigBlind = Convert.ToInt32(nudBigBlindAmnt.Value);
+            m_NbPlayer = Convert.ToInt32(nudNbPlayers.Value);
+            m_WaitingTimeAfterPlayerAction = Convert.ToInt32(nudWTAPlayerAction.Value);
+            m_WaitingTimeAfterBoardDealed = Convert.ToInt32(nudWTABoardDealed.Value);
+            m_WaitingTimeAfterPotWon = Convert.ToInt32(nudWTAPotWon.Value);
+            m_Limit = (TypeBet)clstGameLimit.SelectedIndex;
+            m_OK = true;
+            Close();
+        }
+    }
+}
