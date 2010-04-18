7cd9c2e31fceac10a3ea189bb1cd38d7d2cefd22
ericmas001 <ericmas001@94369782-0f85-11df-ac45-33b0ac6a2133>
2010-04-18 17:14:59 +0000

    C# BluffinPokerServer presque totalement magiquement fonctionnel :)

---
+++ C#Integration/BluffinPokerServer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BluffinPokerServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServerLobby server = new ServerLobby(4242);
+            server.Start();
+            Console.WriteLine("Server started");
+        }
+    }
+}
---
+++ C#Integration/BluffinPokerServer/ServerTableManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using PokerWorld.Game;
+using System.Threading;
+using PokerProtocol.Commands.Lobby;
+using PokerProtocol;
+
+namespace BluffinPokerServer
+{
+    public class ServerTableManager
+    {
+        private readonly int m_NoPort;
+        private readonly PokerGame m_Game;
+        private readonly TcpListener m_SocketServer;
+
+        public PokerGame Game
+        {
+            get { return m_Game; }
+        } 
+
+        public ServerTableManager(PokerGame game, int port)
+        {
+            m_NoPort = port;
+            m_Game = game;
+            m_SocketServer = new TcpListener(port);
+        }
+        public void Start()
+        {
+            m_SocketServer.Start();
+            new Thread(new ThreadStart(Run)).Start();
+        }
+
+        private void Run()
+        {
+            while (true)
+            {
+                try
+                {
+                    TcpClient client = m_SocketServer.AcceptTcpClient();
+                    ServerClientTableManager lobby = new ServerClientTableManager(client, this);
+                    lobby.Start();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.StackTrace);
+                }
+            }
+        }
+    }
+}
