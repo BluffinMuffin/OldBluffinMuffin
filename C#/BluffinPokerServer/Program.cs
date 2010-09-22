95a92602c82084a181ba0dc38266e0fe1f0a0b20
ericmas001 <Ericmas001@users.noreply.github.com>
2010-09-22 22:36:03 +0000

    C#: Server accept arguments [-p port] !

--- C#/BluffinPokerServer/Program.cs
+++ C#/BluffinPokerServer/Program.cs
@@ -8,9 +8,27 @@ namespace BluffinPokerServer
     {
         static void Main(string[] args)
         {
-            ServerLobby server = new ServerLobby(4242);
-            server.Start();
-            Console.WriteLine("Server started");
+            if ((args.Length % 2) == 0)
+            {
+                try
+                {
+                    Dictionary<string, string> map = new Dictionary<string, string>();
+                    for (int i = 0; i < args.Length; i += 2)
+                        map.Add(args[i].ToLower(), args[i + 1]);
+                    int port = 4242;
+                    if (map.ContainsKey("-p"))
+                        port = int.Parse(map["-p"]);
+                    ServerLobby server = new ServerLobby(port);
+                    server.Start();
+                    Console.WriteLine("Server started on port " + port);
+                }
+                catch
+                {
+                    Console.WriteLine("ERROR: Can't start server !!");
+                }
+            }
+            else
+                Console.WriteLine("ERROR: incorrect args");
         }
     }
 }
