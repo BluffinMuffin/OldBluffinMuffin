37ad01ac28c5f159215f9bf38c9acc29fc4b8c82
ericmas001@gmail.com <Ericmas001@users.noreply.github.com>
2014-02-09 18:19:32 +0000

    Using the new RaiseEvent by Reflection of CommandObserver: http://code.google.com/p/ericutility/source/detail?r=161

--- C#/PokerProtocol/Observer/LobbyServerCommandObserver.cs
+++ C#/PokerProtocol/Observer/LobbyServerCommandObserver.cs
@@ -4,11 +4,14 @@ using EricUtility.Networking.Commands;
 using PokerProtocol.Commands.Lobby;
 using PokerProtocol.Commands.Lobby.Training;
 using PokerProtocol.Commands.Lobby.Career;
+using System.Reflection;
 
 namespace PokerProtocol.Observer
 {
     public class LobbyServerCommandObserver : CommandObserver
     {
+        protected override char Delimitter { get { return AbstractLobbyCommand.Delimitter; } }
+
         public event EventHandler<CommandEventArgs<DisconnectCommand>> DisconnectCommandReceived = delegate { };
         public event EventHandler<CommandEventArgs<ListTableCommand>> ListTableCommandReceived = delegate { };
         public event EventHandler<CommandEventArgs<JoinTableCommand>> JoinTableCommandReceived = delegate { };
@@ -25,41 +28,6 @@ namespace PokerProtocol.Observer
         public event EventHandler<CommandEventArgs<CheckDisplayExistCommand>> CheckDisplayExistCommandReceived = delegate { };
         public event EventHandler<CommandEventArgs<AuthenticateUserCommand>> AuthenticateUserCommandReceived = delegate { };
         public event EventHandler<CommandEventArgs<GetUserCommand>> GetUserCommandReceived = delegate { };
-        
-        protected override void receiveSomething(string line)
-        {
-            StringTokenizer token = new StringTokenizer(line, AbstractLobbyCommand.Delimitter);
-            string commandName = token.NextToken();
-            
-            if (commandName == DisconnectCommand.COMMAND_NAME)
-                DisconnectCommandReceived(this, new CommandEventArgs<DisconnectCommand>(new DisconnectCommand(token)));
-            else if (commandName == ListTableCommand.COMMAND_NAME)
-                ListTableCommandReceived(this, new CommandEventArgs<ListTableCommand>(new ListTableCommand(token)));
-            else if (commandName == JoinTableCommand.COMMAND_NAME)
-                JoinTableCommandReceived(this, new CommandEventArgs<JoinTableCommand>(new JoinTableCommand(token)));
-            else if (commandName == GameCommand.COMMAND_NAME)
-                GameCommandReceived(this, new CommandEventArgs<GameCommand>(new GameCommand(token)));
 
-            //Training
-            else if (commandName == CreateTrainingTableCommand.COMMAND_NAME)
-                CreateTrainingTableCommandReceived(this, new CommandEventArgs<CreateTrainingTableCommand>(new CreateTrainingTableCommand(token)));
-            else if (commandName == IdentifyCommand.COMMAND_NAME)
-                IdentifyCommandReceived(this, new CommandEventArgs<IdentifyCommand>(new IdentifyCommand(token)));
-
-            //Career
-            else if (commandName == CreateCareerTableCommand.COMMAND_NAME)
-                CreateCareerTableCommandReceived(this, new CommandEventArgs<CreateCareerTableCommand>(new CreateCareerTableCommand(token)));
-            else if (commandName == CreateUserCommand.COMMAND_NAME)
-                CreateUserCommandReceived(this, new CommandEventArgs<CreateUserCommand>(new CreateUserCommand(token)));
-            else if (commandName == CheckUserExistCommand.COMMAND_NAME)
-                CheckUserExistCommandReceived(this, new CommandEventArgs<CheckUserExistCommand>(new CheckUserExistCommand(token)));
-            else if (commandName == CheckDisplayExistCommand.COMMAND_NAME)
-                CheckDisplayExistCommandReceived(this, new CommandEventArgs<CheckDisplayExistCommand>(new CheckDisplayExistCommand(token)));
-            else if (commandName == AuthenticateUserCommand.COMMAND_NAME)
-                AuthenticateUserCommandReceived(this, new CommandEventArgs<AuthenticateUserCommand>(new AuthenticateUserCommand(token)));
-            else if (commandName == GetUserCommand.COMMAND_NAME)
-                GetUserCommandReceived(this, new CommandEventArgs<GetUserCommand>(new GetUserCommand(token)));
-            
-        }
     }
 }
