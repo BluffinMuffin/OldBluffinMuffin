515be204bc21b3e003d05be76c9a0eecef27f8a1
ericmas001@gmail.com <ericmas001@gmail.com@94369782-0f85-11df-ac45-33b0ac6a2133>
2014-02-23 01:46:41 +0000

    Using the new "LogInConsole" from LogManager !!

--- trunk/C#/BluffinPokerClient/Program.cs
+++ trunk/C#/BluffinPokerClient/Program.cs
@@ -23,55 +23,10 @@ namespace BluffinPokerClient
         }
         static void LogManager_MessageLogged(string from, string message, int level)
         {
-            ConsoleColor c = Console.ForegroundColor;
-
-            if (level >= (int)LogLevel.Error)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                message = "ERROR: " + message;
-            }
-            else if (level >= (int)LogLevel.ErrorLow)
-            {
-                Console.ForegroundColor = ConsoleColor.DarkRed;
-            }
-            else if (level >= (int)LogLevel.Warning)
-            {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                message = "WARNING: " + message;
-            }
-            else if (level >= (int)LogLevel.WarningLow)
-            {
-                Console.ForegroundColor = ConsoleColor.DarkYellow;
-            }
-            else if (level >= (int)LogLevel.MessageVeryHigh)
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-                message = "IMPORTANT: " + message;
-            }
-            else if (level >= (int)LogLevel.Message)
-            {
-                Console.ForegroundColor = ConsoleColor.White;
-            }
-            else if (level >= (int)LogLevel.MessageLow)
-            {
-                Console.ForegroundColor = ConsoleColor.Cyan;
-                message = "DEBUG: " + message;
-            }
-            else
-            {
-                Console.ForegroundColor = ConsoleColor.DarkCyan;
-                message = "DEBUG: " + message;
-            }
-
-            // ATTENTION: This if must contain "LogLevel.Message" for RELEASE
-            //                                 "LogLevel.MessageLow" for DEBUGGING
-            //                                 "LogLevel.MessageVeryLow" for XTREM DEBUGGING
-            if (level >= (int)LogLevel.Message)
-                Console.WriteLine(message);
-
-
-
-            Console.ForegroundColor = c;
+            // ATTENTION: This must contain "LogLevel.Message" for RELEASE
+            //                              "LogLevel.MessageLow" for DEBUGGING
+            //                              "LogLevel.MessageVeryLow" for XTREM DEBUGGING
+            LogManager.LogInConsole(from, message, level, LogLevel.Message);
         }
     }
 }
--- trunk/C#/BluffinPokerServer/Program.cs
+++ trunk/C#/BluffinPokerServer/Program.cs
@@ -36,73 +36,10 @@ namespace BluffinPokerServer
 
         static void LogManager_MessageLogged(string from, string message, int level)
         {
-            ConsoleColor fc = Console.ForegroundColor;
-            ConsoleColor bc = Console.BackgroundColor;
-
-
-            //Errors
-            if (level >= (int)LogLevel.ErrorHigh)
-            {
-                Console.BackgroundColor = ConsoleColor.Red;
-                Console.ForegroundColor = ConsoleColor.White;
-                message = "ERROR: " + message;
-            }
-            if (level >= (int)LogLevel.Error)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                message = "ERROR: " + message;
-            }
-            else if (level >= (int)LogLevel.ErrorLow)
-            {
-                Console.ForegroundColor = ConsoleColor.DarkRed;
-            }
-
-
-            //Warnings
-            else if (level >= (int)LogLevel.Warning)
-            {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                message = "WARNING: " + message;
-            }
-            else if (level >= (int)LogLevel.WarningLow)
-            {
-                Console.ForegroundColor = ConsoleColor.DarkYellow;
-            }
-
-
-            //Messages
-            else if (level >= (int)LogLevel.MessageVeryHigh)
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-                message = "IMPORTANT: " + message;
-            }
-            else if (level >= (int)LogLevel.Message)
-            {
-                Console.ForegroundColor = ConsoleColor.White;
-            }
-            else if (level >= (int)LogLevel.MessageLow)
-            {
-                Console.ForegroundColor = ConsoleColor.Cyan;
-                message = "DEBUG: " + message;
-            }
-            else
-            {
-                Console.ForegroundColor = ConsoleColor.DarkCyan;
-                message = "DEBUG: " + message;
-            }
-
-
-
-            // ATTENTION: This "if" must contain "LogLevel.Message" for RELEASE
-            //                                   "LogLevel.MessageLow" for DEBUGGING
-            //                                   "LogLevel.MessageVeryLow" for XTREM DEBUGGING
-            if (level >= (int)LogLevel.MessageVeryLow)
-                Console.WriteLine(message);
-
-
-
-            Console.ForegroundColor = fc;
-            Console.BackgroundColor = bc;
+            // ATTENTION: This must contain "LogLevel.Message" for RELEASE
+            //                              "LogLevel.MessageLow" for DEBUGGING
+            //                              "LogLevel.MessageVeryLow" for XTREM DEBUGGING
+            LogManager.LogInConsole(from, message, level, LogLevel.Message);
         }
     }
 }
