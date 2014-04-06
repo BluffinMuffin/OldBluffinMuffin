618597750d2dd4e17d4ae96ced7371480b5c6cd0
ericmas001@gmail.com <ericmas001@gmail.com@94369782-0f85-11df-ac45-33b0ac6a2133>
2014-04-06 12:33:29 +0000

    Com.Ericmas001.Game.BluffinMuffin.Server

---
+++ trunk/C#/Com.Ericmas001.Game.BluffinMuffin.Server/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EricUtility;
+
+namespace Com.Ericmas001.Game.BluffinMuffin.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LogManager.MessageLogged += new LogDelegate(LogManager_MessageLogged);
+            if ((args.Length % 2) == 0)
+            {
+                try
+                {
+
+                    Dictionary<string, string> map = new Dictionary<string, string>();
+                    for (int i = 0; i < args.Length; i += 2)
+                        map.Add(args[i].ToLower(), args[i + 1]);
+                    int port = 4242;
+                    if (map.ContainsKey("-p"))
+                        port = int.Parse(map["-p"]);
+                    ServerLobby server = new ServerLobby(port);
+                    server.Start();
+                    LogManager.Log(LogLevel.Message, "BluffinMuffin.Server", "Server started on port {0}", port);
+                }
+                catch
+                {
+                    LogManager.Log(LogLevel.Error, "Program.Main", "Can't start server !!");
+                }
+            }
+            else
+                LogManager.Log(LogLevel.Error, "Program.Main", "Incorrect number of application arguments");
+        }
+
+        static void LogManager_MessageLogged(string from, string message, int level)
+        {
+            // ATTENTION: This must contain "LogLevel.Message" for RELEASE
+            //                              "LogLevel.MessageLow" for DEBUGGING
+            //                              "LogLevel.MessageVeryLow" for XTREM DEBUGGING
+            LogManager.LogInConsole(from, message, level, LogLevel.Message);
+        }
+    }
+}
