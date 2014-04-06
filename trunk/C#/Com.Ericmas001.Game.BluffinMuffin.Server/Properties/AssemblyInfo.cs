80b330db950d0f417ddaec0d4bc9d1273e8a07bf
ericmas001@gmail.com <ericmas001@gmail.com@94369782-0f85-11df-ac45-33b0ac6a2133>
2014-04-06 12:39:39 +0000

    AssemblyInfo of BluffinMuffin.Server

--- trunk/C#/Com.Ericmas001.Game.BluffinMuffin.Server/Properties/AssemblyInfo.cs
+++ trunk/C#/Com.Ericmas001.Game.BluffinMuffin.Server/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Com.Ericmas001.Game.BluffinMuffin.Server")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("BluffinMuffin Server")]
+[assembly: AssemblyDescription("Server for the BluffinMuffin Poker Engine")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Com.Ericmas001.Game.BluffinMuffin.Server")]
+[assembly: AssemblyCompany("Ericmas001.com")]
+[assembly: AssemblyProduct("BluffinMuffin Server")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
