45d46ec1573a0e15073eace39ee09095a5c95d5e
ericmas001@gmail.com <ericmas001@gmail.com@94369782-0f85-11df-ac45-33b0ac6a2133>
2014-04-06 12:47:27 +0000

    Com.Ericmas001.Game.BluffinMuffin.Client

---
+++ trunk/C#/Com.Ericmas001.Game.BluffinMuffin.Client/LobbyTrainingForm.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Com.Ericmas001.Game.Poker.Protocol.Client;
+using Com.Ericmas001.Game.Poker.GUI.Lobby;
+using Com.Ericmas001.Game.Poker.GUI;
+using Com.Ericmas001.Game.Poker.GUI.Game;
+using Com.Ericmas001.Game.Poker.DataTypes.Enums;
+
+namespace Com.Ericmas001.Game.BluffinMuffin.Client
+{
+    public partial class LobbyTrainingForm : Form
+    {
+        private LobbyTCPClient m_Server;
+        public LobbyTrainingForm(LobbyTCPClient server)
+        {
+            m_Server = server;
+            m_Server.ServerLost += new DisconnectDelegate(m_Server_ServerLost);
+            InitializeComponent();
+            tableList.setServer(m_Server);
+            Text = server.PlayerName + " ~ " + lblTitle.Text;
+            lblPlayerName.Text = server.PlayerName;
+            tableList.RefreshList();
+        }
+
+        public delegate void EmptyDelegate();
+        void m_Server_ServerLost()
+        {
+            if (InvokeRequired)
+            {
+                // We're not in the UI thread, so we need to call BeginInvoke
+                BeginInvoke(new EmptyDelegate(m_Server_ServerLost), new object[] { });
+                return;
+            }
+            m_Server = null;
+            Close();
+        }
+        public void AllowJoinOrLeave()
+        {
+            bool selected = tableList.SomethingSelected;
+            GameClient client = tableList.FindClient();
+            btnJoinTable.Enabled = selected && (client == null);
+            btnLeaveTable.Enabled = selected && (client != null);
+        }
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            tableList.RefreshList();
+            AllowJoinOrLeave();
+        }
+
+        private void btnAddTable_Click(object sender, EventArgs e)
+        {
+            tableList.AddTable(LobbyTypeEnum.Training);
+        }
+
+        private void btnJoinTable_Click(object sender, EventArgs e)
+        {
+            AllowJoinOrLeave();
+            tableList.JoinSelected();
+        }
+
+        private void btnLeaveTable_Click(object sender, EventArgs e)
+        {
+            tableList.LeaveSelected();
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            tableList.LeaveAll();
+            Close();
+        }
+
+        private void LobbyForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (m_Server != null)
+                m_Server.Disconnect();
+            //Program.WForm.Show();
+        }
+
+        private void tableList_OnChoiceMade(object sender, EventArgs e)
+        {
+            AllowJoinOrLeave();
+            tableList.JoinSelected();
+        }
+
+        private void tableList_OnSelectionChanged(object sender, EventArgs e)
+        {
+            AllowJoinOrLeave();
+        }
+
+        private void LobbyTrainingForm_Load(object sender, EventArgs e)
+        {
+            if (tableList.NbTables == 0)
+                tableList.AddTable(LobbyTypeEnum.Training);
+        }
+    }
+}
---
+++ trunk/C#/Com.Ericmas001.Game.BluffinMuffin.Client/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using EricUtility;
+using Com.Ericmas001.Game.BluffinMuffin.Client.Menu;
+
+namespace Com.Ericmas001.Game.BluffinMuffin.Client
+{
+    public static class Program
+    {
+        /// <summary>
+        /// Point d'entrée principal de l'application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            LogManager.MessageLogged += new LogDelegate(LogManager_MessageLogged);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MenuForm());
+        }
+        static void LogManager_MessageLogged(string from, string message, int level)
+        {
+            // ATTENTION: This must contain "LogLevel.Message" for RELEASE
+            //                              "LogLevel.MessageLow" for DEBUGGING
+            //                              "LogLevel.MessageVeryLow" for XTREM DEBUGGING
+            LogManager.LogInConsole(from, message, level, LogLevel.Message);
+        }
+    }
+}
